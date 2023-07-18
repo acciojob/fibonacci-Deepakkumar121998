b6833ee34430d66a2aca4ca393e76dfb986cccd8
Deepakkumar121998 <130857964+Deepakkumar121998@users.noreply.github.com>
2023-07-19 00:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let prev1 = 0;
+    let prev2 = 1;
+    let fibNum;
+
+    for (let i = 2; i <= num; i++) {
+      fibNum = prev1 + prev2;
+      prev1 = prev2;
+      prev2 = fibNum;
+    }
+
+    return fibNum;
+  }
 }
 
 module.exports = fibonacci;
