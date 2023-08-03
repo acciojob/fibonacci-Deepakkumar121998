5986f6df521ca93841275fc5b627044859b1a8dc
Deepakkumar121998 <130857964+Deepakkumar121998@users.noreply.github.com>
2023-08-04 01:23:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ function fibonacci(num) {
   if (num === 0) return 0;
   if (num === 1) return 1;
 
-  let prev = 0;
-  let current = 1;
+  let prev1 = 0;
+  let prev2 = 1;
+  let current = 0;
 
   for (let i = 2; i <= num; i++) {
-    const next = prev + current;
-    prev = current;
-    current = next;
+    current = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = current;
   }
 
   return current;
 }
 
-// Test the function with an example
-const n = 6; // Replace this with any positive integer
-const result = fibonacci(n);
-console.log(result);
+// Test cases
+console.log(fibonacci(0)); // Output: 0
+console.log(fibonacci(1)); // Output: 1
+console.log(fibonacci(5)); // Output: 5
+console.log(fibonacci(10)); // Output: 55
+console.log(fibonacci(20)); // Output: 6765
